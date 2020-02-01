8b904836e6e825a14b52e97be1e33abc22e24b0a
FIRE <barry7577@gmail.com>
2020-02-01 14:50:28 +0800

    加入專案檔案。

---
+++ Wpf_InductiveMethod/MainWindow.xaml.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+
+using Point = System.Drawing.Point;
+
+namespace Wpf_InductiveMethod
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ImageUpdate()
+        {
+            mat.SetTo(new byte[600 * 600 * 3]);
+            foreach (InteractObject o in obj)
+                o.drawOn(mat);
+            image1.Source = BitmapSourceConvert.ToBitmapSource(mat);
+        }
+
+        InteractObject[] obj = new InteractObject[2];
+        Mat mat = new Mat(600, 600, DepthType.Cv8U, 3);
+        int Generation = 0;
+        private void Button_start_Click(object sender, RoutedEventArgs e)
+        {
+            Random rnd = new Random();
+            obj[0] = new InteractObject();
+            obj[0].Center = new Point(rnd.Next(50, 550), rnd.Next(50, 550));
+            obj[0].Radius = 40;
+            obj[0].Shape = InteractObject.Type.square;
+            obj[0].Color = new MCvScalar(200, 50, 50);
+            obj[0].trajectory.Clear();
+            obj[0].trajectory.Add(new Trajectory());
+
+            obj[1] = new InteractObject();
+            obj[1].Center = new Point(rnd.Next(50, 550), rnd.Next(50, 550));
+            obj[1].Radius = 40;
+            obj[1].Shape = InteractObject.Type.circle;
+            obj[1].Color = new MCvScalar(50, 200, 50);
+            obj[1].trajectory.Clear();
+            obj[1].trajectory.Add(new Trajectory());
+
+            Generation = 0;
+            tb_checkNum.Text = "Gen : " + Generation.ToString();
+            ImageUpdate();
+        }
+
+        private void Image1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            foreach (InteractObject o in obj)
+            {
+                if (o.isInArea(e.GetPosition(image1)))
+                {
+                    o.pick();
+                    o.trajectory[Generation].AddPoint((e.GetPosition(image1).toDraw()));
+                }
+                else//debug
+                    o.place();
+
+            }
+            ImageUpdate();
+        }
+
+        private void Image1_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            foreach (InteractObject o in obj)
+            {
+                if (o.isInArea(e.GetPosition(image1)))
+                {
+                    o.place();
+                    o.trajectory[Generation].AddPoint((e.GetPosition(image1).toDraw()));
+                }
+            }
+            ImageUpdate();
+        }
+
+        private void Image1_MouseMove(object sender, MouseEventArgs e)
+        {
+            var P = e.GetPosition(image1);
+            foreach (InteractObject o in obj)
+            {
+                if (o.isPick)
+                {
+                    o.Center = new Point((int)P.X, (int)P.Y);
+                    o.trajectory[Generation].AddPoint((e.GetPosition(image1).toDraw()));
+                    ImageUpdate();
+                }
+            }
+        }
+
+        private void Btn_replay_Click(object sender, RoutedEventArgs e)
+        {
+            mat.SetTo(new byte[600 * 600 * 3]);
+            foreach (InteractObject o in obj)
+                o.trajectory[tb_replay_Gen.Text.toInt()].drawOn(mat, o.Color);
+
+            image1.Source = BitmapSourceConvert.ToBitmapSource(mat);
+        }
+        private void Btn_replay_all_Click(object sender, RoutedEventArgs e)
+        {
+            mat.SetTo(new byte[600 * 600 * 3]);
+            foreach (InteractObject o in obj)
+                foreach (Trajectory t in o.trajectory)
+                    t.drawOn(mat, o.Color);
+
+            image1.Source = BitmapSourceConvert.ToBitmapSource(mat);
+        }
+
+        private void Btn_check_Click(object sender, RoutedEventArgs e)
+        {
+            Random rnd = new Random();
+
+            foreach (InteractObject o in obj)
+            {
+                o.Center = new Point(rnd.Next(50, 550), rnd.Next(50, 550));
+                o.trajectory.Add(new Trajectory());
+            }
+            Generation++;
+            tb_checkNum.Text = "Gen : " + Generation.ToString();
+            ImageUpdate();
+        }
+
+        private void Btn_countKeyPoint_Click(object sender, RoutedEventArgs e)
+        {
+
+            for (int Tc = 1; Tc < obj[0].trajectory.Count; Tc++)
+                CompareTwoTrajectory(obj[0].trajectory[0], obj[0].trajectory[Tc], 20);
+
+            mat.SetTo(new byte[600 * 600 * 3]);
+
+            //draw path
+            foreach (InteractObject o in obj)
+                foreach (Trajectory t in o.trajectory)
+                    t.drawOn(mat, o.Color);
+
+            //draw key point      
+            obj[0].trajectory[0].drawKeyOn(1, mat, new MCvScalar(30, 30, 200));
+
+            image1.Source = BitmapSourceConvert.ToBitmapSource(mat);
+
+        }
+
+        private void CompareTwoTrajectory(Trajectory T0, Trajectory Tc, int distance)
+        {
+            for (int P = 0; P < T0.Absolute.Count; P++)
+            {
+                for (int Pc = 0; Pc < Tc.Absolute.Count; Pc++)//compare with T1
+                    if (Ex.Distanse(T0.Absolute[P], Tc.Absolute[Pc]) < distance)//如果T0P0 與 T1Pc0 距離小於   則
+                    {
+                        T0.Abs_keyCount[P]++;
+                        break;//若比對正確，一條路徑只會算一次(each Tc counts once)
+                    }
+            }
+        }
+    }
+
+    public static class BitmapSourceConvert
+    {
+        [DllImport("gdi32")]
+        private static extern int DeleteObject(IntPtr o);
+
+        public static BitmapSource ToBitmapSource(IImage image)
+        {
+            using (System.Drawing.Bitmap source = image.Bitmap)
+            {
+                IntPtr ptr = source.GetHbitmap(); //obtain the Hbitmap
+
+                BitmapSource bs = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                    ptr,
+                    IntPtr.Zero,
+                    Int32Rect.Empty,
+                    System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+
+                DeleteObject(ptr); //release the HBitmap
+                return bs;
+            }
+        }
+    }
+}
---
+++ Wpf_InductiveMethod/object.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+
+using Point = System.Drawing.Point;
+namespace Wpf_InductiveMethod
+{
+    class InteractObject
+    {
+        public enum Type
+        {
+            circle = 0,
+            square = 1
+        }
+
+        public Type Shape = Type.circle;
+        public Point Center = new Point();
+        public int Radius = 30;
+       public MCvScalar Color { get; set; } = new MCvScalar(150, 150, 150);
+
+        private bool _isPick = false;
+        public bool isPick
+        {
+            get { return _isPick; }
+        }
+
+        public void pick()
+        {
+            _isPick = true;
+        }
+        public void place()
+        {
+            _isPick = false;
+        }
+        public bool isInArea(Point point)
+        {
+            int distX = (point.X) - (Center.X);
+            int distY = (point.Y) - (Center.Y);
+
+            if (Shape == Type.circle)
+            {
+                double result = Math.Sqrt(distX * distX + distY * distY);
+                if (result <= 30)
+                    return true;
+                else
+                    return false;
+            }
+            else if (Shape == Type.square)
+            {
+                if (point.X < Center.X + Radius / 2 && point.X > Center.X - Radius / 2 && point.Y > Center.Y - Radius / 2 && point.Y < Center.Y + Radius / 2)
+                    return true;
+                else
+                    return false;
+            }
+
+            return false;
+        }
+        public bool isInArea(System.Windows.Point wPoint)
+        {
+            Point point = new Point((int)wPoint.X, (int)wPoint.Y);
+            return isInArea(point);
+        }
+        public void drawOn(IInputOutputArray img)
+        {
+            if (Shape == Type.circle)
+            {
+                CvInvoke.Circle(img, Center, Radius, Color, -1);
+                if (isPick)
+                    CvInvoke.Circle(img, Center, Radius, new MCvScalar(Color.V0 + 50, Color.V1 + 50, Color.V2 + 50), 3);
+            }
+            else if (Shape == Type.square)
+            {
+                CvInvoke.Rectangle(img, new System.Drawing.Rectangle(Center.X - Radius / 2, Center.Y - Radius / 2, Radius, Radius), Color, -1);
+                if (isPick)
+                {
+                    CvInvoke.Line(img, new Point(Center.X - Radius / 2, Center.Y - Radius / 2), new Point(Center.X - Radius / 2, Center.Y + Radius / 2), new MCvScalar(Color.V0 + 50, Color.V1 + 50, Color.V2 + 50), 3);
+                    CvInvoke.Line(img, new Point(Center.X + Radius / 2, Center.Y + Radius / 2), new Point(Center.X - Radius / 2, Center.Y + Radius / 2), new MCvScalar(Color.V0 + 50, Color.V1 + 50, Color.V2 + 50), 3);
+                    CvInvoke.Line(img, new Point(Center.X + Radius / 2, Center.Y + Radius / 2), new Point(Center.X + Radius / 2, Center.Y - Radius / 2), new MCvScalar(Color.V0 + 50, Color.V1 + 50, Color.V2 + 50), 3);
+                    CvInvoke.Line(img, new Point(Center.X - Radius / 2, Center.Y - Radius / 2), new Point(Center.X + Radius / 2, Center.Y - Radius / 2), new MCvScalar(Color.V0 + 50, Color.V1 + 50, Color.V2 + 50), 3);
+                }
+
+            }
+        }
+
+        //--Trajectory--//
+        public List<Trajectory> trajectory = new List<Trajectory>();
+
+        public void IniTrajectory()
+        {
+
+        }
+
+    }
+
+
+    public class Trajectory
+    {
+        public List<Point> Absolute = new List<Point>();
+        public List<int> Abs_keyCount = new List<int>();
+        public List<Point> Relative = new List<Point>();
+        public List<int> Rel_keyCount = new List<int>();
+
+        public void Clear()
+        {
+            Absolute.Clear();
+            Relative.Clear();
+        }
+
+        public void AddPoint(Point P)
+        {
+            Absolute.Add(P);
+            Abs_keyCount.Add(0);
+        }
+        public void resetKeyCount()
+        {
+            for (int c = 0; c < Abs_keyCount.Count; c++)
+                Abs_keyCount[c] = 0;
+        }
+        public void drawOn(IInputOutputArray img, MCvScalar Color, int Radius = 3)
+        {
+            for (int i = 0; i < Absolute.Count(); i++)
+            {
+                CvInvoke.Circle(img, Absolute[i], Radius, Color, -1);
+            }
+        }
+        /// <summary>
+        /// draw if >= threshold
+        /// </summary>
+
+        public void drawKeyOn(int threshold, IInputOutputArray img, MCvScalar Color, int Radius = 3)
+        {
+            for (int i = 0; i < Absolute.Count(); i++)
+            {
+                if (Abs_keyCount[i] >= threshold)
+                    CvInvoke.Circle(img, Absolute[i], Radius, Color, -1);
+            }
+        }
+
+    }
+
+
+
+    static class Ex
+    {
+        static public Point toDraw(this System.Windows.Point wPoint)
+        {
+            return new Point((int)wPoint.X, (int)wPoint.Y);
+        }
+        static public int toInt(this string str)
+        {
+            return int.Parse(str);
+        }
+        static public int Distanse(Point P1 ,Point P2)
+        {
+            int dx = P2.X - P1.X;
+            int dy = P2.Y - P1.Y;
+
+            double D = Math.Sqrt((dx * dx) +(dy * dy));
+            return (int)D;
+        }
+    }
+}
